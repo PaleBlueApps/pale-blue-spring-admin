bb8743ab3032e0fa0f074147511c807f045159ce
ABeesettiPaleBlue <150929658+ABeesettiPaleBlue@users.noreply.github.com>
2025-11-13 10:39:07 +0530

    Bump version from 1.0.0 to 1.1.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -58,7 +58,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.paleblueapps",
         artifactId = "springadmin",
-        version = "1.0.0",
+        version = "1.1.0",
     )
 
     pom {
