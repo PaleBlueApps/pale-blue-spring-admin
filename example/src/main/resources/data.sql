95dde8eee3726f71985162803af143cc36e0ed5b
Biplab Dutta <bdutta@paleblueapps.com>
2025-11-19 03:22:56 +0545

    update initial seed data

--- example/src/main/resources/data.sql
+++ example/src/main/resources/data.sql
@@ -1,5 +1,12 @@
-INSERT INTO users (id, username, email, age)
-VALUES
-    (1, 'biplab', 'biplab@example.com', 27),
-    (2, 'veronica', 'veronica@example.com', 25),
-    (3, 'georgia', 'georgia@example.com', 23);
+CREATE TABLE IF NOT EXISTS users
+(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER
+);
+
+INSERT INTO users (id, username, email, age) VALUES (1, 'biplab', 'biplab@example.com', 25);
+INSERT INTO users (id, username, email, age) VALUES (2, 'anil', 'anil@example.com', 26);
+INSERT INTO users (id, username, email, age) VALUES (3, 'michael', 'michael@example.com', 27);
+INSERT INTO users (id, username, email, age) VALUES (4, 'mike', 'mike@example.com', 28);
