a62e403391bb199478a3ba7730fbecd3590ea230
Biplab Dutta <bdutta@paleblueapps.com>
2025-11-19 04:28:19 +0545

    avoid inserting the same items in the sample db when launching example project

--- example/src/main/resources/data.sql
+++ example/src/main/resources/data.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users
     age INTEGER
 );
 
-INSERT INTO users (id, username, email, age) VALUES (1, 'biplab', 'biplab@example.com', 25);
-INSERT INTO users (id, username, email, age) VALUES (2, 'anil', 'anil@example.com', 26);
-INSERT INTO users (id, username, email, age) VALUES (3, 'michael', 'michael@example.com', 27);
-INSERT INTO users (id, username, email, age) VALUES (4, 'mike', 'mike@example.com', 28);
+INSERT OR IGNORE INTO users (id, username, email, age) VALUES (1, 'biplab', 'biplab@example.com', 25);
+INSERT OR IGNORE INTO users (id, username, email, age) VALUES (2, 'anil', 'anil@example.com', 26);
+INSERT OR IGNORE INTO users (id, username, email, age) VALUES (3, 'michael', 'michael@example.com', 27);
+INSERT OR IGNORE INTO users (id, username, email, age) VALUES (4, 'mike', 'mike@example.com', 28);
