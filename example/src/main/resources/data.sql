debe8e003e1831fe881726a3266bd7eaa15ec1ce
Biplab Dutta <bdutta@paleblueapps.com>
2025-11-19 01:29:16 +0545

    add a sql query to seed initial data

---
+++ example/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, username, email, age)
+VALUES
+    (1, 'biplab', 'biplab@example.com', 27),
+    (2, 'veronica', 'veronica@example.com', 25),
+    (3, 'georgia', 'georgia@example.com', 23);
