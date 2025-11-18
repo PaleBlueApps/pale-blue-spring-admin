0f7d3a86f43e35658e97cf79b501313edffb3f1a
Biplab Dutta <bdutta@paleblueapps.com>
2025-11-19 03:25:12 +0545

    clean up User entity

--- example/src/main/kotlin/com/paleblueapps/springadminexample/example/entity/User.kt
+++ example/src/main/kotlin/com/paleblueapps/springadminexample/example/entity/User.kt
@@ -1,20 +1,21 @@
 package com.paleblueapps.springadminexample.example.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "users")
 data class User(
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
-
     @Column(nullable = false)
     val username: String,
-
     @Column(nullable = false, unique = true)
     val email: String,
-
-    val age: Int
+    val age: Int,
 )
